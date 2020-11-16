1e7188f5ef1e3fc8039cafd18993c12dc5cf9b56
Makoto-9501 <grandmap.pro@gmail.com>
2020-11-16 20:48:44 +0900

    add herokuenv

--- dbconnect.php
+++ dbconnect.php
@@ -7,13 +7,18 @@ class DbManager
     public function connect()
     {
         //DBに接続
-        $host = "localhost";
-        // データベース名を変えた場合はこちらを変更する（$dbname = "データベース名"）
-        $dbname = "todo";
-        $charset = "utf8mb4";
-        $user = 'root';
-        $password = '';
-        $options = [
+        // $host = "localhost";
+        // // データベース名を変えた場合はこちらを変更する（$dbname = "データベース名"）
+        // $dbname = "todo";
+        // $charset = "utf8mb4";
+        // $user = 'root';
+        // $password = '';
+        // $options = [
+        $host = getenv('host'); //MySQLがインストールされてるコンピュータ
+        $dbname = getenv('dbname'); //使用するDB
+        $charset = "utf8"; //文字コード
+        $user = getenv('username'); //MySQLにログインするユーザー名
+        $password = getenv('password'); //ユーザーのパスワード
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
